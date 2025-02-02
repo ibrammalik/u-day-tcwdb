ef8ef49ef750be370223a0635e9f40c5a170cfca
ibrammalik <malikmaul1173@gmail.com>
2025-02-02 21:11:41 +0700

    Drum Kit
    Add animation and change kit order

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -6,47 +6,57 @@ const tom2Sound = new Audio('./sounds/tom-2.mp3');
 const tom3Sound = new Audio('./sounds/tom-3.mp3');
 const tom4Sound = new Audio('./sounds/tom-4.mp3');
 
-document.addEventListener('keypress', (event) => {
+document.addEventListener('keydown', (event) => {
   playSound(event.key);
+  buttonAnimation(event.key);
 });
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', (event) => {
-    const key = event.target.classList[0];
+    const key = event.target.innerHTML;
     playSound(key);
+    buttonAnimation(key);
   });
 });
 
 function playSound(key) {
   switch (key) {
     case 'w':
-      crashSound.currentTime = 0;
-      crashSound.play();
-      break;
-    case 'a':
-      kickBassSound.currentTime = 0;
-      kickBassSound.play();
-      break;
-    case 's':
-      snareSound.currentTime = 0;
-      snareSound.play();
-      break;
-    case 'd':
       tom1Sound.currentTime = 0;
       tom1Sound.play();
       break;
-    case 'j':
+    case 'a':
       tom2Sound.currentTime = 0;
       tom2Sound.play();
       break;
-    case 'k':
+    case 's':
       tom3Sound.currentTime = 0;
       tom3Sound.play();
       break;
-    case 'l':
+    case 'd':
       tom4Sound.currentTime = 0;
       tom4Sound.play();
+      break;
+    case 'j':
+      snareSound.currentTime = 0;
+      snareSound.play();
+      break;
+    case 'k':
+      crashSound.currentTime = 0;
+      crashSound.play();
+      break;
+    case 'l':
+      kickBassSound.currentTime = 0;
+      kickBassSound.play();
   }
+}
+
+function buttonAnimation(key) {
+  const activeButton = document.querySelector('.' + key);
+  activeButton.classList.toggle('pressed');
+  setTimeout(() => {
+    activeButton.classList.toggle('pressed');
+  }, 100);
 }
